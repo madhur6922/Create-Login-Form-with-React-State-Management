7bdb6b3665a9dd241e8b42229116454c16429b5d
madhur6922 <madhursharma6922@gmail.com>
2025-10-30 09:53:12 +0530

    Initialize App component with LoginForm

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h2>React Login Form</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
